00c139488546f11fc0ad526ae6a8d6d367a95acd
adair.schmitt <adair.schmitt@MU-C1-18.mullen.pvt.k12.co.us>
2024-09-30 13:16:23 -0600

    added worthless comment

--- Mastermind/src/mastermind.java
+++ Mastermind/src/mastermind.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 public class mastermind
+//testing committ please
 	{
-
+		static String [][] board= new String [9][3];
 		public static void main(String[] args)
 			{
+				
 				greet();
 				instructions();
+				displayBoard();
+				prepBoard();
+				goPlay();
 				
 			}
 		public static void greet()
@@ -31,6 +36,70 @@ public class mastermind
 					System.out.println("3. Color is not in code");
 					System.out.println("You will get 9 guesses to guess the code.");
 				}
+			else if (instruct==2)
+				{
+				goPlay();	
+				}
+			
+			
+		}
+		public static void prepBoard() 
+			{ 
+				for (int row=0; row<board.length; row++)
+				      {
+				        for(int col=0; col<board[0].length; col++)
+				          {
+				            board [row][col]= " ";
+				          }
+				      }
+			}
+		public static void displayBoard()
+		{
+			System.out.print("   1   2   3 ");
+		    System.out.println();
+		    System.out.print(" -------------");
+		    System.out.println();
+		    System.out.print("A| "+board[0][0]+" | "+board[0][1]+" | "+board[0][2]+" |");
+		    System.out.println();
+		    System.out.print(" -------------");
+		    System.out.println();
+		    System.out.print("B| "+board[1][0]+" | "+board[1][1]+" | "+board[1][2]+" |");
+		    System.out.println();
+		    System.out.print(" -------------");
+		    System.out.println();
+		    System.out.print("C| "+board[2][0]+" | "+board[2][1]+" | "+board[2][2]+" |");
+		    System.out.println();
+		    System.out.print(" -------------");
+		    System.out.println();
+		    System.out.print("A| "+board[3][0]+" | "+board[3][1]+" | "+board[3][2]+" |");
+		    System.out.println();
+		    System.out.print(" -------------");
+		    System.out.println();
+		    System.out.print("B| "+board[4][0]+" | "+board[4][1]+" | "+board[4][2]+" |");
+		    System.out.println();
+		    System.out.print(" -------------");
+		    System.out.println();
+		    System.out.print("C| "+board[5][0]+" | "+board[5][1]+" | "+board[5][2]+" |");
+		    System.out.println();
+		    System.out.print(" -------------");
+		    System.out.println();	
+		    System.out.print("A| "+board[3][0]+" | "+board[3][1]+" | "+board[3][2]+" |");
+		    System.out.println();
+		    System.out.print(" -------------");
+		    System.out.println();
+		    System.out.print("B| "+board[4][0]+" | "+board[4][1]+" | "+board[4][2]+" |");
+		    System.out.println();
+		    System.out.print(" -------------");
+		    System.out.println();
+		    System.out.print("C| "+board[5][0]+" | "+board[5][1]+" | "+board[5][2]+" |");
+		    System.out.println();
+		    System.out.print(" -------------");
+		    System.out.println();	
+		}
+			
+		}
+		public static void goPlay()
+		{
 			
 		}
 
